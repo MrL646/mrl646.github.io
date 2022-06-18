ea5916c7bdcbbea487ae6302eb1b4ee50cd37117
MrL646 <mrliu646@foxmail.com>
2022-06-18 08:22:13 +0800

    Update chk.js

--- chk.js
+++ chk.js
@@ -1,7 +1,5 @@
 var host = window.location.host;
-if (host=="mirrors-zhs141.vercel.app") 
-{ 
+if (host=="mirrors-zhs141.vercel.app")
+{
     window.location.replace("https://zhs141.vercel.app");
-} else {
-    var "project-name" = "MrLiu646的主页"
 }
